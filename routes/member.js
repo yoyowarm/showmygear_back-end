e89e6c70b67b7943e7505373d98d30827ffb2bd4
yoyowarmli <yoyowarmli@jetfuel.com.tw>
2021-02-24 11:15:33 +0800

    project init

---
+++ routes/member.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/', function(req, res, next) {
+  console.log(req.body.test)
+  const memberData = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    create_date: onTime()
+  }
+});
+
+const onTime = () => {
+  const date = new Date();
+    const mm = date.getMonth() + 1;
+    const dd = date.getDate();
+    const hh = date.getHours();
+    const mi = date.getMinutes();
+    const ss = date.getSeconds();
+
+    return [date.getFullYear(), "-" +
+        (mm > 9 ? '' : '0') + mm, "-" +
+        (dd > 9 ? '' : '0') + dd, " " +
+        (hh > 9 ? '' : '0') + hh, ":" +
+        (mi > 9 ? '' : '0') + mi, ":" +
+        (ss > 9 ? '' : '0') + ss
+    ].join('');
+}
+module.exports = router;
